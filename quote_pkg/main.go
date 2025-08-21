8979f213dabf63768fc962e5efd3ca2aa7823d63
Malik Ahmad Rasheed <malikahmad6666666@gmail.com>
2025-08-21 16:24:16 +0500

    get packages from GO dev like quote to print different quotes for glass go or opt

---
+++ quote_pkg/main.go
@@ -0,0 +1,13 @@
+package main // Execute the main function
+
+import (
+	"fmt"
+
+	"rsc.io/quote/v4" // Print Quote pkg 
+)
+
+func main() {
+	//fmt.Println(quote.Go())
+	//fmt.Println(quote.Glass())
+	fmt.Println(quote.Opt())
+}
