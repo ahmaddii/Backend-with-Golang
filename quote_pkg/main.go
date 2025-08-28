5581a27f11cafca803b1b7f543a3caf080cb3fad
Malik Ahmad Rasheed <malikahmad6666666@gmail.com>
2025-08-28 20:43:12 +0500

    Fix formatting and add comment for quote response in main function

--- quote_pkg/main.go
+++ quote_pkg/main.go
@@ -3,11 +3,13 @@ package main // Execute the main function
 import (
 	"fmt"
 
-	"rsc.io/quote/v4" // Print Quote pkg 
+	"rsc.io/quote/v4" // Print Quote pkg
 )
 
 func main() {
 	//fmt.Println(quote.Go())
 	//fmt.Println(quote.Glass())
 	fmt.Println(quote.Opt())
+
+	// how it respond everything
 }
