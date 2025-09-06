29044f7346ff0958a15a9cf64024e5e8b76da2b8
Malik Ahmad Rasheed <malikahmad6666666@gmail.com>
2025-09-06 18:24:56 +0500

    Learn Maps how we use loop inside them and also how we delete it and how we can pick one key value

--- maps/main.go
+++ maps/main.go
@@ -16,8 +16,53 @@ func main() {
 
 	myMap["Banna"] = 25 // same there also
 
+	myMap["Apple"] = 50 // Update the Value of Apple Key
+
+	myMap["Orange"] = 60
+
 	fmt.Println(myMap) // show full key value pair  maps
 
 	fmt.Println(myMap["Apple"]) // show only value using key
+
+	// Check weather the key is available or not
+
+	value,exsist := myMap["Apple"]
+
+	if exsist {
+
+		fmt.Println("Found the Key",value)
+	} else {
+
+		fmt.Println("Not Found")
+	}
+
+	// Deleting Keys
+
+	delete(myMap,"Orange")
+
+	fmt.Println("After Deleting Orange Key")
+
+	fmt.Println(myMap)
+
+
+	// Loop through the Map
+
+	for key,value := range myMap{
+
+		fmt.Println("Key: ",key, "Value :",value)
+
+
+	}
+
+	// Example of Headers in using go maps
+
+	headers := map[string]string{
+
+		"content-type": "Application/json",
+		"Authorization": "Bearer token 123",
+
+	}
+
+	fmt.Println("Authorization: ",headers["Authorization"])
 
 }
